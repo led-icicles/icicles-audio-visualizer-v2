421416866390343afb69113966bb7381438967de
gonuit <kamilklyta341@gmail.com>
2021-12-27 16:34:25 +0100

    update wave codec

--- src/renderer/utils/codecs/wave_codec.ts
+++ src/renderer/utils/codecs/wave_codec.ts
@@ -30,55 +30,45 @@ export class WaveCodec extends Codec {
 
     this.baseLevelTransformer = new BaseLevelTransformer();
     this.panelDisabledColor = options?.panelDisabledColor ?? new Color();
-    this.panelEnabledColor = options?.panelEnabledColor ?? Colors.white;
-    this.rows = new Array(this.yCount)
+    this.panelEnabledColor = options?.panelEnabledColor ?? Colors.red;
+    this.rows = new Array(this.xCount)
       .fill(undefined)
-      .map(() => new Array(this.xCount).fill(0));
+      .map(() => new Array(this.yCount).fill(0));
   }
 
   public animate(audioBins: Uint8Array, baseLevel: number): AnimationView {
-    const baseValue = this.baseLevelTransformer.transform(baseLevel);
-
+    const level = this.baseLevelTransformer.transform(baseLevel);
     const radioPanelColor = Color.linearBlend(
       this.panelDisabledColor,
       this.panelEnabledColor,
-      baseValue
+      level
     );
     const updatedRadioPanels = this.radioPanels.map((panel) =>
       panel.copyWith({ color: radioPanelColor })
     );
-    const audioLevels = new Array(this.xCount / 2);
-    const binsPerLevel = Math.floor(audioBins.length / (this.xCount * 2));
 
-    for (let levelIndex = 0; levelIndex < this.xCount; levelIndex++) {
-      const start = levelIndex * binsPerLevel;
-      const end = start + binsPerLevel;
+    const half = Math.floor(this.yCount / 2);
+    const value = Math.floor(baseLevel * half);
 
-      let sum = 0;
-      for (let binIndex = start; binIndex < end; binIndex++) {
-        sum += audioBins[binIndex];
-      }
-      const avg = sum / binsPerLevel;
-      const level = avg / 255;
-      audioLevels[levelIndex] = level;
+    const audioLevels = new Array(this.xCount).fill(0);
+
+    for (let i = 0; i < half; i++) {
+      const leftIndex = half - i - 1;
+      const rightIndex = half + i;
+      const validLevel = i < value ? 1 : 0;
+      audioLevels[leftIndex] = validLevel;
+      audioLevels[rightIndex] = validLevel;
     }
 
-    const half = Math.floor(this.xCount / 2);
-    const centeredLevels = audioLevels
-      .map((_, index) =>
-        index < half ? audioLevels[half - 1 - index] : audioLevels[index - half]
-      )
-      .map((a) => (a - 0.8 < 0 ? 0 : a));
-
-    this.rows.unshift(centeredLevels);
-    this.rows.pop();
+    this.rows.push(audioLevels);
+    this.rows.shift();
 
     this.icicles.setAllPixelsColor(new Color());
     const frame = this.icicles.toFrame(Codec.minFrameDuration);
 
     for (let x = 0; x < this.xCount; x++) {
       for (let y = 0; y < this.yCount; y++) {
-        const color = this.rows[y][x];
+        const color = this.rows[x][y];
         const ledIndex = this.icicles.getPixelIndex(x, y);
         frame.pixels[ledIndex] = Color.linearBlend(
           new Color(0, 0, 0),
